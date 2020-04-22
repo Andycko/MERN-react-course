84c439503f81e606788779268f6d48d0881e5918
Andrej Szalma <andrej@fortytwo.studio>
2020-04-22 20:58:34 +0100

    added basic routes

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
       await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 
-      console.log("MongoDB connected")
+      console.log("MongoDB connected...")
   } catch(err) {
       console.error(err.message);
       // Exit process with failure
