c243ca33deea0707156f6289724c5ae23a09952a
Andrej Szalma <andrej@fortytwo.studio>
2020-06-03 15:33:15 +0100

    added Profile model and finished profile routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => res.send('API Running'));
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profiles', require('./routes/api/profiles'));
+app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
@@ -21,3 +21,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT} *_*`);
 });
+
